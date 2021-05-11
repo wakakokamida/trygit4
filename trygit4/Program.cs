76c2952d4306223d175e14db5a017ad7d7834d5a
wakako <wakako@occ.co.jp>
2021-05-11 14:06:28 +0900

    add hello

--- trygit4/Program.cs
+++ trygit4/Program.cs
@@ -7,6 +7,8 @@ namespace trygit4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
